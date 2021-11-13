b30635326b63aa558b510fd0b321f95ed74d7bf0
Edward Krizheskiy <ekrizhev@uncc.edu>
2021-11-12 19:23:35 -0500

    Update Admin

--- boxr/main/admin.py
+++ boxr/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Style,Size,Color,Carton_QTY, Product,Pallets,Products_On_Pallets
+from .models import Style,Size,Color,Carton_QTY, Product,Pallets,Products_On_Pallets,Location,Restock
 
 admin.site.register(Size)
 admin.site.register(Style)
@@ -9,3 +9,5 @@ admin.site.register(Carton_QTY)
 admin.site.register(Product)
 admin.site.register(Pallets)
 admin.site.register(Products_On_Pallets)
+admin.site.register(Location)
+admin.site.register(Restock)
